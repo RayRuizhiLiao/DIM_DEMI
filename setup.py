295245ee19e6c0a920394a91938189f142250c55
Ruizhi Liao <ruizhi@csail.mit.edu>
2020-11-16 20:38:08 -0500

    checked nce loss

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='cortex_DIM_DEMI',
-      version='0.14',
+setup(name='cortex_DIM_DEMI_dev',
+      version='0.18',
       description='The Deep InfoMax package',
       author='R Devon Hjelm',
       author_email='erroneus@gmail.com',
-      packages=['cortex_DIM_DEMI', 'cortex_DIM_DEMI.configs', 'cortex_DIM_DEMI.functions', 'cortex_DIM_DEMI.nn_modules',
-                'cortex_DIM_DEMI.models', 'cortex_DIM_DEMI.evaluation_models'],
+      packages=['cortex_DIM_DEMI_dev', 'cortex_DIM_DEMI_dev.configs', 'cortex_DIM_DEMI_dev.functions', 'cortex_DIM_DEMI_dev.nn_modules',
+                'cortex_DIM_DEMI_dev.models', 'cortex_DIM_DEMI_dev.evaluation_models'],
       install_requires=['cortex==0.13a0'],
       zip_safe=False)
